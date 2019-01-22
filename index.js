e492742891fc07b2100ed58e4729ca307a8ce29c
Ryan Jasinski <jasinski.17@gmail.com>
2019-01-22 16:03:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+  var object2 = object;
+  object2[key] = value;
+  return object2;
 }
 
