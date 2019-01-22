6e8e8a5b1dfa26eb907089cc299dc88d6f2a1bd0
Ryan Jasinski <jasinski.17@gmail.com>
2019-01-22 16:07:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
+function deleteFromObjectByKey(object, key) {
+  var object2 = Object.assign({},object)
+  delete object2[key];
+  return object2;
+}
+
